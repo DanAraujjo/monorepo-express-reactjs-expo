4f77eedcf686107590f97e1a11a64966341ff906
DanAraujjo <dan.araujjo@gmail.com>
2020-01-20 14:20:28 -0300

    monorepo

---
+++ packages/server/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("Servidor on-line!");
+});
+
+app.listen(3333);
